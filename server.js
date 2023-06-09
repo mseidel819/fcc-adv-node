cf05a861202e56ccc212b66dda9af1cff7cc6e93
Matt Seidel <mseidel819@gmail.com>
2023-06-09 13:21:54 -0500

    dotenv

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 "use strict";
-require("dotenv").config();
+require("dotenv").config({ path: "./sample.env" });
 const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
@@ -13,6 +15,17 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route("/").get((req, res) => {
   res.render("index", { title: "Hello", message: "Please log in" });
