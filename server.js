e31554cbba09e2bfc35a2c844cd9361516a2950b
Matt Seidel <mseidel819@gmail.com>
2023-06-10 09:21:54 -0500

    chore: register

--- server.js
+++ server.js
@@ -29,18 +29,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
-      return done(null, user);
-    });
-  })
-);
-
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
 
@@ -50,7 +38,8 @@ myDB(async (client) => {
     res.render("index", {
       title: "Connected to Database",
       message: "Please login",
-      showLogin: true,
+      // showLogin: true,
+      showRegistration: true,
     });
   });
 
@@ -63,13 +52,6 @@ myDB(async (client) => {
       }
     );
 
-  function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/");
-  }
-
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
     res.render("profile", { username: req.user.username });
   });
@@ -79,9 +61,53 @@ myDB(async (client) => {
     res.redirect("/");
   });
 
+  app.route("/register").post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: req.body.password,
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    }
+  );
+
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
   });
+
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    })
+  );
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
@@ -97,6 +123,13 @@ myDB(async (client) => {
     res.render("index", { title: e, message: "Unable to connect to database" });
   });
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
