e3caa2c4af58db660f3bfc3920300a0f6e2461bd
Matt Seidel <mseidel819@gmail.com>
2023-06-10 09:08:23 -0500

    chore: add logout

--- server.js
+++ server.js
@@ -79,6 +79,15 @@ app.route("/profile").get(ensureAuthenticated, (req, res) => {
   res.render("profile", { username: req.user.username });
 });
 
+app.route("/logout").get((req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+app.use((req, res, next) => {
+  res.status(404).type("text").send("Not Found");
+});
+
 passport.serializeUser((user, done) => {
   done(null, user._id);
 });
