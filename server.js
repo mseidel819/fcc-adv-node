ee5e7e5bacbe36fc2a1af251ad23b8307019cc19
Matt Seidel <mseidel819@gmail.com>
2023-06-10 09:17:40 -0500

    fix:catch moved

--- server.js
+++ server.js
@@ -53,48 +53,48 @@ myDB(async (client) => {
       showLogin: true,
     });
   });
+
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
+
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/");
+  }
+
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    res.render("profile", { username: req.user.username });
+  });
+
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("index", { title: e, message: "Unable to connect to database" });
-  });
-});
-
-app
-  .route("/login")
-  .post(
-    passport.authenticate("local", { failureRedirect: "/" }),
-    (req, res) => {
-      res.redirect("/profile");
-    }
-  );
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/");
-}
-
-app.route("/profile").get(ensureAuthenticated, (req, res) => {
-  res.render("profile", { username: req.user.username });
-});
-
-app.route("/logout").get((req, res) => {
-  req.logout();
-  res.redirect("/");
-});
-
-app.use((req, res, next) => {
-  res.status(404).type("text").send("Not Found");
-});
-
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, doc);
   });
 });
 
