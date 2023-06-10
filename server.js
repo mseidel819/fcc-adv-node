cdcc44a85bdef7fd011245703900c23ba8781272
Matt Seidel <mseidel819@gmail.com>
2023-06-10 09:04:53 -0500

    chore:next

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ function ensureAuthenticated(req, res, next) {
 }
 
 app.route("/profile").get(ensureAuthenticated, (req, res) => {
-  res.render("profile");
+  res.render("profile", { username: req.user.username });
 });
 
 passport.serializeUser((user, done) => {
